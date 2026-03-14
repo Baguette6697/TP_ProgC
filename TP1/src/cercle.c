0ef9122ea18ca50acf229bdc2bb4f352b820b6dd
Baguette6697 <clement.bouveret@2028.icam.fr>
2026-03-14 18:20:27 +0100

    Exercice 1.2 done

--- TP1/src/cercle.c
+++ TP1/src/cercle.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
 #include <string.h>
+#include <math.h>
 
 int main() {
-double pi = 3.1416;
-double r = 6;  
-    printf("L'air du cercle = %f", pi*(r*r));
-   printf("Le périmètre du cercle = %f", pi*(r+r));
+    double rayon;
+    printf("Entrez le rayon de votre cercle et je vous donnerais son aire et son perimetre\n"
+           "rayon en [mm] =");
+
+    scanf("%lf", &rayon);
+
+    double aire_cercle = M_PI * rayon * rayon;
+    double perimetre = (2 * rayon) * M_PI;
+
+    printf("\nl'air de votre cercle est de %.4f [mm²]\n", aire_cercle);
+    printf("le permietre de votre cerlce est de %.4f [mm]\n", perimetre);
+
 }
 
