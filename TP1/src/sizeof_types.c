2d0763d98b1deb898fece1a1178b03ac1131ef4e
Baguette6697 <clement.bouveret@2028.icam.fr>
2026-03-15 12:20:49 +0100

    Exercice 1.3 done

--- TP1/src/sizeof_types.c
+++ TP1/src/sizeof_types.c
@@ -3,16 +3,17 @@
 int main()
 {
     // titre des colonnes
-    printf("%-3s | %-15s | %-15s\n", "#", "type", "nombre d'octets");
-    printf("--------------------------------------\n");
+    printf("%-3s | %-15s | %-25s | %-25s\n", "#", "type", "nombre d'octets signe", "nombre d'oct non-signe");
+    printf("-----------------------------------------------------------------\n");
 
     // contenue du tableau
-    printf("1   | %-15s | %-15zu\n", "char", sizeof(char));
-    printf("2   | %-15s | %-15zu\n", "short", sizeof(short));
-    printf("3   | %-15s | %-15zu\n", "int", sizeof(int));
-    printf("4   | %-15s | %-15zu\n", "long int", sizeof(long int));
-    printf("5   | %-15s | %-15zu\n", "long long int", sizeof(long long int));
-    printf("6   | %-15s | %-15zu\n", "float", sizeof(float));
-    printf("7   | %-15s | %-15zu\n", "double", sizeof(double));
-    printf("8   | %-15s | %-15zu\n", "long double", sizeof(long double));
+    printf("1   | %-15s | %-25zu | %-25zu\n", "char", sizeof(signed char), sizeof(unsigned char));
+    printf("2   | %-15s | %-25zu | %-25zu\n", "short", sizeof(signed short), sizeof(unsigned short));
+    printf("3   | %-15s | %-25zu | %-25zu\n", "int", sizeof(signed int), sizeof(unsigned int));
+    printf("4   | %-15s | %-25zu | %-25zu\n", "long int", sizeof(signed long int), sizeof(unsigned long int));
+    printf("5   | %-15s | %-25zu | %-25zu\n", "long long int", sizeof(signed long long int), sizeof(unsigned long long int));
+    printf("-------cas ou il n'y a pas de version 'signed' ou 'unsigned'-----\n");
+    printf("6   | %-15s | %-25zu | %-25zu\n", "float", sizeof(float), sizeof(float));
+    printf("7   | %-15s | %-25zu | %-25zu\n", "double", sizeof(double), sizeof(double));
+    printf("8   | %-15s | %-25zu | %-25zu\n", "long double", sizeof(long double), sizeof(long double));
 }
