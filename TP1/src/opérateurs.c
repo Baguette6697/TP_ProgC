f7ea207db52e2f99794faef723400a145f164af9
Baguette6697 <clement.bouveret@2028.icam.fr>
2026-03-15 14:01:29 +0100

    Exercice 1.5 partially done

--- TP1/src/opérateurs.c
+++ TP1/src/opérateurs.c
@@ -1,2 +1,19 @@
+#include <stdio.h>
 
+int main()
+{
+
+    int a = 16;
+    int b = 3;
+
+    printf("Addition : a+b=%d \n", a+b);
+    printf("Sourstraction : a-b=%d \n", a-b);
+    printf("Multiplication : a+b=%d \n", a+b);
+    printf("Division : a/b=%.4f \n", (float)a / (float)b);
+    printf("Modulo : a+b=%d \n", a+b);
+    printf("Comparaison : a>b ? : %s \n", a>b);
+
+    return 0;
+
+}
 
---
+++ TP1/src/variables.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char valeur_int
+    int
+
+}
