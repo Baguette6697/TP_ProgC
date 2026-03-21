475bb09962b03f224b2101cf814226523321ab3b
Baguette6697 <clement.bouveret@2028.icam.fr>
2026-03-21 15:19:00 +0100

    Exercice 1.6 done.

--- TP1/src/boucles.c
+++ TP1/src/boucles.c
@@ -4,15 +4,18 @@
 
 int main()
 {
-    srand(time(NULL));
+    srand(time(NULL)); //On genere un nombre aleatoire pour notre triangle.
     int compteur = (rand() % 9) + 2;
-    int i=0;
-    for (i=0; i<compteur; i++)
+    int i, j; // on declare des variable qui vont nous servir de compteur d'iteration. i pour les colonnes et j pour le nombre d'etoiles par lignes.
+    
+    for (i = 0; i < compteur; i++) //premiere boucle 'for' pour commencer une ligne.
     {
-        char character_par_defaut = '*';
-        for ();
-        printf("*");
+        for (j = 0; j <= i; j++) // deuxieme boucle 'for' pour le nombres d'etoiles par lignes
+        {
+            printf("*");
+        }
+        printf("\n"); // on oublie pas de sauter une ligne...
     }
-    printf("%d",compteur);
+    printf("%d", compteur);
     return 0;
 }
