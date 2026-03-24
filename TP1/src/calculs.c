ac2e6eda47d1464dec96fe55f40eeee555283fe0
Baguette6697 <clement.bouveret@2028.icam.fr>
2026-03-24 13:23:29 +0100

    Exercice 1.8 partially done.

--- TP1/src/calculs.c
+++ TP1/src/calculs.c
@@ -1,1 +1,60 @@
+#include <stdio.h>
 
+
+int main()
+{
+    int num1;
+    int num2;
+    char op;
+
+    printf("Rentrez au clavier le type d'operation que vous voudriez faire : \n options :'+', '-', '*', '/', '%', '&', '|', '~'");
+    scanf("\n %c", &op);
+
+    printf("Rentrez le premier nombre au clavier puis appuyez sur 'enter' :");
+    scanf("\n %d", &num1);
+
+    printf("Rentrez le second nombre au clavier puis appuyez sur 'enter' :");
+    scanf("\n %d", &num2);
+
+    switch (op)
+    {
+        case '+':
+            int result = num1+num2;
+            printf("%d+%d=%d",num1, num2, result);
+            return result;
+
+        case '-':
+            int result = num1-num2;
+            printf("%d-%d=%d",num1, num2, result);
+            return result;
+
+        case '*':
+            int result = num1*num2;
+            printf("%d*%d=%d",num1, num2, result);
+            return result;
+
+        case '/':
+            int result = num1/num2;
+            printf("%d/%d=%d",num1, num2, result);
+            return result;
+
+        case '&':
+            int result = num1&num2;
+            printf("%d&%d=%d",num1, num2, result);
+            return result;
+
+        case '|':
+            int result = num1|num2;
+            printf("%d|%d=%d",num1, num2, result);
+            return result;
+
+        case '~':
+            int result = num1~num2;
+            printf("%d~%d=%d",num1, num2, result);
+            return result;
+
+        default:
+            printf("\n Operation impossible. Veuillez reessayer. \n *attention!* \n la fonction n'accepte que des nombres entier.");
+    }
+    return 0;
+}
